b2732081ebf59919d41a84569d8a9f85588b9918
coderius <sunrise4fun@gmail.com>
2019-09-26 13:26:48 +0000

    Update widget view query params and js vars

--- widgets/hitCounter/views/invisible-counter.php
+++ widgets/hitCounter/views/invisible-counter.php
@@ -4,25 +4,28 @@
  * -------------
  * 
  * Js vars
- * var Cd - contains dom document.
- * var Cr - counter random integer. Needed for generate unique src. So that the browser does not cache the picture  
- * var Cp - counter query params
- * var Cnv- contains navigator
+ * var Cd   - contains dom document.
+ * var Cr   - counter random integer. Needed for generate unique src. So that the browser does not cache the picture  
+ * var Cp   - counter query params
+ * var Cn   - contains navigator
+ * var Cn_c - Connection type detected by js
  * 
  * Query params:
- * param c     - flag. If cookei enabled = 1 or if not -not set.
- * param j     - flag. if java enabled =1 if not -not set.
- * param t     - time zone offset. For example, if your time zone is UTC+10 (Australian Eastern Standard Time), -600 will be returned. 
- * param i     - counter id
- * param u     - returns the href (URL) of the current page
- * param r     - returns referef href
- * param w     - screen width
- * param h     - screen height
- * param d     - The colorDepth property returns the bit depth of the color palette for displaying images (in bits per pixel)
- * param lg    - Language of the browser
- * param hl    - History size current session in this window
- * param td    - Detect toutch device
- * param Cnv_con- Connection type detected by js
+ * param c      - flag. If cookei enabled = 1 or if not -not set.
+ * param j      - flag. if java enabled =1 if not -not set.
+ * param t      - time zone offset. For example, if your time zone is UTC+10 (Australian Eastern Standard Time), -600 will be returned. 
+ * param i      - counter id
+ * param u      - returns the href (URL) of the current page
+ * param r      - returns referef href
+ * param w      - screen width
+ * param h      - screen height
+ * param d      - The colorDepth property returns the bit depth of the color palette for displaying images (in bits per pixel)
+ * param lg     - Language of the browser
+ * param hl     - History size current session in this window
+ * param td     - Detect toutch device
+ * param 
+ * param ram    - Ram computer
+ * param tz     - Intl time zone
  * 
  * -----------------
  * Expressions in js
@@ -31,26 +34,43 @@
  * Cd.cookie="dtct=abc";if(Cd.cookie)Cp+="&c=1";
  * 
  * //Find out if your browser has Java enabled:
- * if(Cnv.javaEnabled())Cp+="&j=1";
+ * if(Cn.javaEnabled())Cp+="&j=1";
  */
 ?>
+
+<?php //Speed profiling ?>
+<?php if(YII_DEBUG): ?>
+<script language="javascript" type="text/javascript">console.time('hitCounter-' + '<?= $counterId; ?>')</script>
+<?php endif; ?>
+
 <script language="javascript" type="text/javascript"><!--
-Cd=document;Cnv=navigator;Cnv_con=Cnv.connection||Cnv.mozConnection||Cnv.webkitConnection;<?php
+<?php
+
+/* Define Variables*/
+
+?>Cd=document;<?php
+?>Cn=navigator;<?php
+?>Cn_c=Cn.connection||Cn.mozConnection||Cn.webkitConnection;<?php
+
+/* Build query params*/
+
 ?>Cr="&"+Math.random();<?php
 ?>Cp="&hl="+history.length;<?php
 ?>Cd.cookie="dtct=abc";<?php
 ?>if(Cd.cookie)Cp+="&c=1";<?php
 ?>Cp+="&t="+(new Date()).getTimezoneOffset();<?php
 ?>if(self!=top)Cp+="&f=1";<?php
-?>if(Cnv.language)Cp+="&lg="+Cnv.language;<?php
-?>if('ontouchstart' in window || Cnv.msMaxTouchPoints)Cp+="&td=1";<?php
-?>Cp+="&cnt="+Cnv_con.effectiveType;<?php
-?>Cp+="&ram="+Cnv.deviceMemory;<?php
-?>//--></script><?php
+?>if(Cn.language)Cp+="&lg="+Cn.language;<?php
+?>if('ontouchstart' in window || Cn.msMaxTouchPoints)Cp+="&td=1";<?php
+?>Cp+="&cnt="+Cn_c.effectiveType;<?php
+?>Cp+="&ram="+Cn.deviceMemory;<?php
+?>if(Intl)Cp+="&tz="+Intl.DateTimeFormat().resolvedOptions().timeZone;<?php
+?>
+//--></script><?php
 
 
 ?><script language="javascript1.1" type="text/javascript"><!--
-if(Cnv.javaEnabled())Cp+="&j=1";
+if(Cn.javaEnabled())Cp+="&j=1";
 //--></script><?php
 
 
@@ -73,3 +93,7 @@ Cd.write("<img src='<?= $imgSrc; ?>?i=<?= $counterId; ?>"+Cp+Cr+
 ?>+"<?= $clientImgOptions ?>/>");
 //--></script>
 
+<?php //Speed profiling ?>
+<?php if(YII_DEBUG): ?>
+<script language="javascript" type="text/javascript">console.timeEnd('hitCounter-' + '<?= $counterId; ?>');</script>
+<?php endif; ?>
