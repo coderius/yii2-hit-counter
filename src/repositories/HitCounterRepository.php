2d2f0867b43c32e98c1a71e5b1c2695e90784013
Sergio <36597893+coderius@users.noreply.github.com>
2020-08-31 22:31:22 +0300

    Update HitCounterRepository.php

--- src/repositories/HitCounterRepository.php
+++ src/repositories/HitCounterRepository.php
@@ -9,7 +9,7 @@ use coderius\hitCounter\entities\HitCounter;
 
 class HitCounterRepository{
 
-    public function save(HitCounter $hit): void
+    public function save(HitCounter $hit)
     {
         if (!$hit->save()) {
             throw new \RuntimeException('Saving error.');
